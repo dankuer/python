e132e656b5ac002dc863343bb669cd003dd6f735
dankuer <dankuer@163.com>
2017-07-01 06:53:18 +0800

    完成myserver

--- MySocketServer.py
+++ MySocketServer.py
@@ -3,7 +3,30 @@
 # MySocketServer.py
 # Created by Dankuer on 2017/7/1
 
+import socket
+import threading
+from datetime import time
+
+
+def tcplink(sock, addr):
+    print('Accept new connection from %s:%s...' % addr)
+    sock.send(b'Welcome!')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+    sock.close()
+    print('Connection from %s:%s closed.' % addr)
 
 
 if __name__ == '__main__':
-    print('Hello World!')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('45.32.52.153', 9999))
+    s.listen(5)
+    print('Server begin listening.')
+    while True:
+        sock, addr = s.accept()
+        t = threading.Thread(target=tcplink, args=(sock, addr))
+        t.start()
